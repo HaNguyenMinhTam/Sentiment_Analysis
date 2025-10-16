4c5ff65f262beca7b4358764368550b6021dbedc
HaNguyenMinhTam <127753137+HaNguyenMinhTam@users.noreply.github.com>
2025-10-16 23:21:24 +0700

    Add script to build Word2Vec features and save embeddings

---
+++ src/features/build_word2vec_features.py
@@ -0,0 +1,53 @@
+import pandas as pd
+from gensim.models import Word2Vec
+import numpy as np
+import os
+
+# Load data 
+train_df = pd.read_csv("D:/Projects/Sentiment_Analysis/data/processed/train_clean.csv")
+dev_df = pd.read_csv("D:/Projects/Sentiment_Analysis/data/processed/dev_clean.csv")
+test_df = pd.read_csv("D:/Projects/Sentiment_Analysis/data/processed/test_clean.csv")
+
+print("Train:", train_df.shape)
+print("Dev:", dev_df.shape)
+print("Test:", test_df.shape)
+
+# Ensure data type and word separation
+train_df['sentence'] = train_df['sentence'].astype(str)
+dev_df['sentence'] = dev_df['sentence'].astype(str)
+test_df['sentence'] = test_df['sentence'].astype(str)
+
+sentences = [text.split() for text in train_df['sentence']]
+dev_sentences = [text.split() for text in dev_df['sentence']]
+test_sentences = [text.split() for text in test_df['sentence']]
+
+# Fit the Word2Vec model
+w2v_model = Word2Vec(
+    sentences=sentences,   # training data
+    vector_size=100,       # vector dimensions (usually 100–300)
+    window=5,              # ngữ cảnh (context size)
+    min_count=2,           # ignore words that appear less than 2 times
+    workers=4,             # number of CPU threads
+    sg=1                   # skip-gram (1) or CBOW (0)
+)
+
+# Function to get the average vector of a sentence
+def get_sentence_vector(tokens, model):
+    valid_vectors = [model.wv[word] for word in tokens if word in model.wv]
+    if len(valid_vectors) == 0:
+        return np.zeros(model.vector_size)
+    return np.mean(valid_vectors, axis=0)
+
+# Represent the entire sentence as a vector
+X_train_w2v = np.array([get_sentence_vector(tokens, w2v_model) for tokens in sentences])
+X_dev_w2v = np.array([get_sentence_vector(tokens, w2v_model) for tokens in dev_sentences])
+X_test_w2v = np.array([get_sentence_vector(tokens, w2v_model) for tokens in test_sentences])
+
+# Save model and feature vector
+w2v_model.save("D:/Projects/Sentiment_Analysis/models/word2vec.model")
+print("✅ Word2Vec model saved as 'word2vec.model'")
+np.save("D:/Projects/Sentiment_Analysis/data/features/Word2Vec/X_train_w2v.npy", X_train_w2v)
+np.save("D:/Projects/Sentiment_Analysis/data/features/Word2Vec/X_dev_w2v.npy", X_dev_w2v)
+np.save("D:/Projects/Sentiment_Analysis/data/features/Word2Vec/X_test_w2v.npy", X_test_w2v)
+
+print("✅ Saved vectorized data: X_train_w2v.npy, X_dev_w2v.npy, X_test_w2v.npy")
